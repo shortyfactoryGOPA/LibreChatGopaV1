18a587aa268a39e5308b596fdb941f6e5bef6027
Arnaud <shortyfactory@gmail.com>
2026-04-30 11:51:21 +0200

    fix(auth): create name memory on OpenID/Azure login for all users

--- api/strategies/openidStrategy.js
+++ api/strategies/openidStrategy.js
@@ -18,7 +18,7 @@ const {
   resolveAppConfigForUser,
 } = require('@librechat/api');
 const { getStrategyFunctions } = require('~/server/services/Files/strategies');
-const { findUser, createUser, updateUser } = require('~/models');
+const { findUser, createUser, updateUser, createMemory } = require('~/models');
 const { getAppConfig } = require('~/server/services/Config');
 const getLogStores = require('~/cache/getLogStores');
 
@@ -695,6 +695,19 @@ async function processOpenIDAuth(tokenset, existingUsersOnly = false) {
   }
 
   user = await updateUser(user._id, user);
+
+  if (fullName && user._id) {
+    createMemory({
+      userId: user._id,
+      key: 'Prénom et nom',
+      value: fullName,
+      tokenCount: Math.ceil(fullName.length / 4),
+    }).catch((err) => {
+      if (!err.message?.includes('already exists')) {
+        logger.warn(`[openidStrategy] Failed to create name memory: ${err.message}`);
+      }
+    });
+  }
 
   logger.info(
     `[openidStrategy] login success openidId: ${user.openidId} | email: ${user.email} | username: ${user.username} `,
